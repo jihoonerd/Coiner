c4968edbf87588c9a6be53fe452425f216f20b89
Jihoon Kim <jihoonerd@outlook.com>
2017-10-03 01:34:04 +0900

    Prototype of 'Trader' class

---
+++ trader/trader.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 10/3/17
+Author: Jihoon Kim
+"""
+
+from account_info import api_key, api_secret
+
+
+class Trader(object):
+
+    def __init__(self, currency, trade_algorithm, api_key=api_key, api_secret=api_secret):
+        self.currency = currency
+        self.trade_algorithm = trade_algorithm
+        self.api_key = api_key
+        self.api_secret = api_secret
