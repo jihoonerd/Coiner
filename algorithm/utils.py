2bacee5ace8387c1852abacbf5a6a2a25eaf9b5b
Jihoon Kim <jihoonerd@outlook.com>
2017-10-04 16:38:32 +0900

    Update Bollinger Band plotting

--- algorithm/utils.py
+++ algorithm/utils.py
@@ -10,6 +10,19 @@ import matplotlib.pyplot as plt
 
 def bollinger_band(table, window=20, std=2, draw=True):
 
+    """
+
+    :param table:
+    :type table:
+    :param window:
+    :type window:
+    :param std:
+    :type std:
+    :param draw:
+    :type draw:
+    :return:
+    :rtype:
+    """
     moving_avg_price = table.Price.rolling(window).mean()
     moving_std_price = table.Price.rolling(window).std()
 
@@ -17,12 +30,13 @@ def bollinger_band(table, window=20, std=2, draw=True):
     lower_band = moving_avg_price - (moving_std_price * std)
 
     if draw:
-        plt.figure(figsize=(800, 600))
-        plt.plot(table.Time, table.Price, label="Price")
-        plt.plot(table.Time, moving_avg_price, label="MA (" + str(window) + ")")
-        plt.plot(table.Time, upper_band, label="Upper Band (" + str(std) + "$\sigma$)")
-        plt.plot(table.Time, lower_band, label="Lower Band (" + str(std) + "$\sigma$)")
+        plt.plot(table.index, table.Price, label="Price")
+        plt.plot(table.index, moving_avg_price, label="MA (" + str(window) + ")")
+        plt.plot(table.index, upper_band, label="Upper Band (" + str(std) + "$\sigma$)")
+        plt.plot(table.index, lower_band, label="Lower Band (" + str(std) + "$\sigma$)")
+        plt.fill_between(table.index, lower_band, upper_band, facecolor='m', alpha=.15)
         plt.legend()
-        plt.title("Bollinger Band (Window=" + str(window) + ")")
+        plt.title("Bollinger Band (Window=" + str(window) + "), Records started on: " +
+                  str(table.Time[0]))
 
     return moving_avg_price, upper_band, lower_band
