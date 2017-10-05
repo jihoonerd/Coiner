a4e1c2cfdbf605880dede5481dac133c08613b1c
Jihoon Kim <jihoonerd@outlook.com>
2017-10-05 10:53:35 +0900

    Docstring of 'bollinger_band'

--- algorithm/utils.py
+++ algorithm/utils.py
@@ -11,17 +11,18 @@ import matplotlib.pyplot as plt
 def bollinger_band(table, window=20, std=2, draw=True):
 
     """
+    Returns Bollinger Bands (MA, Upper_band, Lower_band)
 
-    :param table:
-    :type table:
-    :param window:
-    :type window:
-    :param std:
-    :type std:
-    :param draw:
-    :type draw:
-    :return:
-    :rtype:
+    :param table: Table recorded by `recorder` method.
+    :type table: pd.DataFrame
+    :param window: Window for Moving Average
+    :type window: int
+    :param std: Standard Deviation for upper/lower band.
+    :type std: float
+    :param draw: Draws Bollinger Band Plot if it set to True
+    :type draw: bool
+    :return: MA, Upper_band, Lower_band
+    :rtype: pd.Series
     """
     moving_avg_price = table.Price.rolling(window).mean()
     moving_std_price = table.Price.rolling(window).std()
@@ -34,7 +35,7 @@ def bollinger_band(table, window=20, std=2, draw=True):
         plt.plot(table.index, moving_avg_price, label="MA (" + str(window) + ")")
         plt.plot(table.index, upper_band, label="Upper Band (" + str(std) + "$\sigma$)")
         plt.plot(table.index, lower_band, label="Lower Band (" + str(std) + "$\sigma$)")
-        plt.fill_between(table.index, lower_band, upper_band, facecolor='m', alpha=.15)
+        plt.fill_between(table.index, lower_band, upper_band, facecolor='k', alpha=.15)
         plt.legend()
         plt.title("Bollinger Band (Window=" + str(window) + "), Records started on: " +
                   str(table.Time[0]))
