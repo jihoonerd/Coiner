a0ad7d3a007bb2d7bea7df72c9d6acb7462615cc
Jihoon Kim <jihoonerd@outlook.com>
2017-10-06 21:52:33 +0900

    Added: 'record_orderbook'
    Now, 'buy_place' and 'sell_place' uses the price in relative price based on orderbook instead of hand-written price.

--- trader/trader.py
+++ trader/trader.py
@@ -8,8 +8,11 @@ import datetime
 import pandas as pd
 import time
 import math
+import json
+import urllib.request
 from account_info import api_key, api_secret
 from api.xcoin_api_client import XCoinAPI
+from api.config import api_url
 from algorithm.utils import bollinger_band
 
 
@@ -21,13 +24,14 @@ class Trader(XCoinAPI):
             self.currency = currency
             self.trade_algorithm = trade_algorithm
             self.table = pd.DataFrame(columns={'Time', 'Price', 'Buy_Price', 'Sell_Price'})
+            self.orderbook = None
             self.last_currency_price = None
             self.available_cur = None
             self.available_krw = None
             self.trade_fee = None
             self.min_trade_cur_decimal = None
-            self.buy_price = None
-            self.sell_price = None
+            self.target_buy_price = None
+            self.target_sell_price = None
             self.trader_buy_price = None
             self.trader_buy_units = None
 
@@ -35,6 +39,7 @@ class Trader(XCoinAPI):
             self.recorder(record=False, report=False)
             self.update_wallet()
             self.set_min_trade_cur_decimal()
+            self.info()
 
         def info(self):
             print("==============[Trader INFO]==============")
@@ -42,8 +47,8 @@ class Trader(XCoinAPI):
             print("Trading Algorithm  : " + str(self.trade_algorithm))
             print("Number of Logs     : " + str(len(self.table)))
             print("Last Currency Value: " + str(self.last_currency_price))
-            print("Buy Price          : " + str(self.buy_price))
-            print("Sell Price         : " + str(self.sell_price))
+            print("Buy Price          : " + str(self.target_buy_price))
+            print("Sell Price         : " + str(self.target_sell_price))
             print("Available Currency : " + str(self.available_cur))
             print("Available KRW      : " + str(self.available_krw))
             print("Trading Fee        : " + str(self.trade_fee))
@@ -87,14 +92,13 @@ class Trader(XCoinAPI):
             """
 
             rg_params = {
-                "currency": self.currency
+
             }
 
+            self.record_orderbook()
             response_recorder = self.xcoinApiCall("/public/ticker/" + self.currency, rg_params)
             status = "OK" if response_recorder["status"] == "0000" else "ERROR"
             last_price = response_recorder["data"]["closing_price"]
-            buy_price = response_recorder["data"]["buy_price"]
-            sell_price = response_recorder["data"]["sell_price"]
             current_time = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
             if report:
@@ -103,18 +107,15 @@ class Trader(XCoinAPI):
                 print("Time  : " + current_time)
                 print("Index : " + str(len(self.table)))
                 print("{0:6s}: ".format(self.currency) + last_price)
-                print("BUY   : " + buy_price)
-                print("SELL  : " + sell_price)
 
             current_time = pd.to_datetime(current_time, format="%Y-%m-%d %H:%M:%S")
             last_price = float(response_recorder["data"]["closing_price"])
             self.last_currency_price = last_price
-            self.buy_price = float(buy_price)
-            self.sell_price = float(sell_price)
+            self.target_buy_price = float(self.orderbook.Buy_Price[2])
+            self.target_sell_price = float(self.orderbook.Sell_Price[6])
 
             if record:
-                new_data = {"Time": current_time, "Price": last_price,
-                            "Buy_Price": buy_price, "Sell_Price": sell_price}
+                new_data = {"Time": current_time, "Price": last_price}
                 self.table = self.table.append(new_data, ignore_index=True)
 
             return self.table
@@ -141,7 +142,7 @@ class Trader(XCoinAPI):
         def buy_place(self, units="ALL"):
 
             if units == "ALL":
-                before_fee_unit = self.available_krw / self.buy_price
+                before_fee_unit = self.available_krw / self.target_buy_price
                 expected_fee = before_fee_unit * self.trade_fee
                 units_buy = math.floor((before_fee_unit - expected_fee) * (10**self.min_trade_cur_decimal))/(10**self.min_trade_cur_decimal)
             else:
@@ -151,7 +152,7 @@ class Trader(XCoinAPI):
                 "order_currency": self.currency,
                 "payment_currency": "KRW",
                 "units": units_buy,
-                "price": int(self.buy_price),
+                "price": int(self.target_buy_price),
                 "type": "bid"
             }
 
@@ -162,8 +163,8 @@ class Trader(XCoinAPI):
                 print("============[PLACE BUY]============")
                 print("Status     : " + status)
                 print("Order    ID: " + response_buy["order_id"])
-                print("Order Price: " + str(self.buy_price))
-                self.trader_buy_price = self.buy_price
+                print("Order Price: " + str(self.target_buy_price))
+                self.trader_buy_price = self.target_buy_price
                 self.trader_buy_units = units_buy
 
             else:
@@ -187,7 +188,7 @@ class Trader(XCoinAPI):
                 "order_currency": self.currency,
                 "payment_currency": "KRW",
                 "units": units_sell,
-                "price": int(self.sell_price),
+                "price": int(self.target_sell_price),
                 "type": "ask"
             }
 
@@ -198,7 +199,7 @@ class Trader(XCoinAPI):
                 print("============[PLACE SELL]============")
                 print("Status     : " + status)
                 print("Order    ID: " + response_sell["order_id"])
-                print("Order Price: " + str(self.sell_price))
+                print("Order Price: " + str(self.target_sell_price))
 
             else:
                 print(response_sell)
@@ -279,15 +280,46 @@ class Trader(XCoinAPI):
 
             return units_sell
 
+        def record_orderbook(self, report=True):
+
+            url_orderbook = urllib.request.urlopen(api_url + '/public/orderbook/' + self.currency)
+            read_orderbook = url_orderbook.read()
+            json_orderbook = json.loads(read_orderbook)
+
+            orderbook = pd.DataFrame(columns={'Buy_Price', 'Buy_Quantity', 'Sell_Price', 'Sell_Quantity'})
+
+            status = "OK" if json_orderbook['status'] == "0000" else "ERROR"
+
+            print("==========[Record Orderbook]==========")
+            print("Time  : " + str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+            print("Status: " + status)
+
+            for i in range(len(json_orderbook["data"]["bids"])):
+                buy_price = json_orderbook["data"]["bids"][i]["price"]
+                buy_quantity = json_orderbook["data"]["bids"][i]["quantity"]
+                sell_price = json_orderbook["data"]["asks"][i]["price"]
+                sell_quantity = json_orderbook["data"]["asks"][i]["quantity"]
+
+                orderbook = orderbook.append({'Buy_Price': buy_price,
+                                              'Buy_Quantity': buy_quantity,
+                                              'Sell_Price': sell_price,
+                                              'Sell_Quantity': sell_quantity}, ignore_index=True)
+
+            self.orderbook = orderbook
+
+            if report:
+                print(self.orderbook.head())
+            return orderbook
+
         def run_trading(self):
 
             buy_limit = self.last_currency_price
 
             while True:
                 self.recorder()
-                if len(self.table) > 20:
+                if len(self.table) > 15:
                     bollinger_avg, bollinger_upper, bollinger_lower = bollinger_band(self.table,
-                                                                                     window=20, std=2,
+                                                                                     window=15, std=2,
                                                                                      draw=False)
 
                     if self.last_currency_price > bollinger_upper.values[-1]:
@@ -300,9 +332,11 @@ class Trader(XCoinAPI):
                     elif self.last_currency_price < bollinger_lower.values[-1]:
                         _, status = self.buy_place()
                         if status == "OK":
-                            buy_limit = self.trader_buy_price * (self.trade_fee * self.trader_buy_units * 6 + 1) + 300
+                            buy_limit = self.trader_buy_price * (self.trade_fee * self.trader_buy_units * 2.5 + 1) + 200
                             print("Set Buy Limit: " + str(buy_limit))
 
-                time.sleep(5)
+                time.sleep(10)
 
             return None
+
+
